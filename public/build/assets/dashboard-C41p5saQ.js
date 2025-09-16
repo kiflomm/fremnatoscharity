d910098c412fc4793ce07c7d7d1a5ade9c1ebd9c
kiflom <kiflomberihu2024@gmail.com>
2025-09-16 18:02:19 +0300

    feat: add new build assets and update manifest
    - Introduced multiple new JavaScript and CSS files in the public/build directory to enhance the application's UI and functionality.
    - Updated the manifest.json to include the new CSS asset for better asset management.
    - Removed outdated CSS file to streamline the build assets.

--- public/build/assets/dashboard-C41p5saQ.js
+++
@@ -1,1 +0,0 @@
-import{r,a}from"./app-18Lfh4wc.js";/* empty css            */function s(){return r.useEffect(()=>{a.visit("/admin/dashboard")},[]),null}export{s as default};
---
+++ public/build/assets/dashboard-CpybTFBz.js
@@ -0,0 +1,1 @@
+import{r,a}from"./app-grFH0y_I.js";/* empty css            */function s(){return r.useEffect(()=>{a.visit("/admin/dashboard")},[]),null}export{s as default};
