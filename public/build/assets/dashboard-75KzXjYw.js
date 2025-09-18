6ad480937050a8b02f61a5d7b06d82547c75dd80
kiflom <kiflomberihu2024@gmail.com>
2025-09-18 10:52:27 +0300

    refactor: update build assets and introduce new components
    - Added multiple new JavaScript and CSS files to enhance the application's UI components, including alert dialogs, layouts, and various icons.
    - Updated manifest.json to include the new assets and removed outdated CSS files for better organization and management.
    - Introduced new components such as user profiles, news layouts, and input elements to improve user experience and maintainability.
    - Enhanced the styling and layout of existing components for improved visual consistency and user interaction.

---
+++ public/build/assets/dashboard-75KzXjYw.js
@@ -0,0 +1,1 @@
+import{r,a}from"./app-DGzFG503.js";/* empty css            */function s(){return r.useEffect(()=>{a.visit("/admin/dashboard")},[]),null}export{s as default};
---
+++ public/build/assets/video-FRd13Kqh.js
@@ -0,0 +1,26 @@
+import{c as o}from"./index-Bd7KM-iR.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M12 5v14",key:"s699le"}],["path",{d:"m19 12-7 7-7-7",key:"1idqje"}]],i=o("ArrowDown",e);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m5 12 7-7 7 7",key:"hav0vg"}],["path",{d:"M12 19V5",key:"x0mq9r"}]],s=o("ArrowUp",c);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M8 12h8",key:"1wcyev"}],["path",{d:"M12 8v8",key:"napkw2"}]],y=o("CirclePlus",t);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["polyline",{points:"17 8 12 3 7 8",key:"t8dd8p"}],["line",{x1:"12",x2:"12",y1:"3",y2:"15",key:"widbto"}]],r=o("Upload",a);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5",key:"ftymec"}],["rect",{x:"2",y:"6",width:"14",height:"12",rx:"2",key:"158x01"}]],p=o("Video",d);export{s as A,y as C,r as U,p as V,i as a};
