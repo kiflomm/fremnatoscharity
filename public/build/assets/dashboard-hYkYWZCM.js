7e9f89fca3884bbce397bdf0262a4e79074c21fa
kiflom <kiflomberihu2024@gmail.com>
2025-09-18 07:25:09 +0300

    refactor: update build assets and introduce new components
    - Added multiple new JavaScript and CSS files to enhance the application's UI components, including alert dialogs, layouts, and various icons.
    - Updated the manifest.json to include the new assets and removed outdated CSS files for better organization and management.
    - Introduced new components such as user profiles, news layouts, and input elements to improve user experience and maintainability.

---
+++ public/build/assets/dashboard-hYkYWZCM.js
@@ -0,0 +1,1 @@
+import{r,a}from"./app-7bm-VKv1.js";/* empty css            */function s(){return r.useEffect(()=>{a.visit("/admin/dashboard")},[]),null}export{s as default};
---
+++ public/build/assets/input-error-JHW1fgCa.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-7bm-VKv1.js";import{a as o}from"./index-Ckig5j1P.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
