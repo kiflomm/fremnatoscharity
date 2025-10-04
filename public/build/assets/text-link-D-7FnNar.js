ebda5b6adf11b0bd7fc2421955202779fd3f8188
kiflom <kiflomberihu2024@gmail.com>
2025-10-04 07:16:46 +0300

    feat: add new asset files and components for improved UI
    - Introduced multiple new JavaScript and CSS asset files to enhance the user interface, including alert dialogs, buttons, and various layout components.
    - Added new components for handling user interactions, such as dialogs and alerts, to improve user experience.
    - Updated the manifest file to include the new assets, ensuring they are properly loaded in the application.

---
+++ public/build/assets/text-link-D-7FnNar.js
@@ -0,0 +1,1 @@
+import{j as t,L as n}from"./app-BNOx4n0Q.js";import{a}from"./index-C8xIoliH.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
